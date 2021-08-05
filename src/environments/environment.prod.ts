68f201e2c74105c04f31dfa86fc60083c7f45475
RAJAT CHAUHAN <rajatchauhan407@gmail.com>
2021-08-05 13:49:39 +0530

    https done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  Url:"backend.rcveg.com"
+  Url:"https://backend.rcveg.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  Url:"backend.rcveg.com",
+  Url:"https://backend.rcveg.com",
   
 };
 /*
